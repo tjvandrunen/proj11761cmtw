fa7615e90fa66fcdc3866214fef3fef325e7fb8c
Wes <hermes@ubuntu.(none)>
2013-04-22 18:34:31 -0400

    updated turboparser installer

--- scripts/install_turbo.sh
+++ scripts/install_turbo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #install_turbo.sh
 #Weston Feely
-#4/17/13
+#4/22/13
 
 #This bash script will download and install TurboParser v2.0.2 in the current directory, along with the necessary model files to run English TurboTagger and TurboParser
 
@@ -15,13 +15,19 @@ cd TurboParser-2.0.2
 ./install_deps.sh
 ./configure && make && make install
 #Make tmp dir in scripts folder
-mkdir scripts/tmp
+mkdir -p scripts/tmp
 #Move into models directory and make english_proj dir
 cd models/
-mkdir english_proj
+mkdir -p english_proj
 #Download and untar English TurboTagger model
 wget http://www.ark.cs.cmu.edu/TurboParser/sample_models/english_proj_tagger.tar.gz
 tar -xvzf english_proj_tagger.tar.gz
+rm english_proj_tagger.tar.gz
 #Download and untar English TurboParser model
 wget http://www.ark.cs.cmu.edu/TurboParser/sample_models/english_proj_parser.tar.gz
 tar -xvzf english_proj_parser.tar.gz
+rm english_proj_parser.tar.gz
+#Export command to use TurboParser
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:`pwd;`/deps/local/lib:"
+#Test TurboParser (comment this line out later)
+#echo "I solved the problem with statistics." | ./scripts/parse.sh
